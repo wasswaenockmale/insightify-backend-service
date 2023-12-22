c3b1547929e09bac3985ca2a8b5e06f99cbb7bf6
wasswaenockmale <enockmale97@gmail.com>
2023-12-22 11:30:51 +0300

    root route

--- index.js
+++ index.js
@@ -9,25 +9,32 @@ const io = new Server(server);
 
 app.use(express.json());
 
+
+app.get('/', (req, res) => {
+  console.log("It works");
+  res.json({
+    'success': 200
+  });
+})
 io.on('connection', () => {
   console.log('user connected');
 });
 
 app.get('/hooks', (req, res) => {
   const payload = req.body;
-  console.log("No, error occuring")
+  console.log("No, error occuring");
   io.emit('notification', { payload });
-  res.json({
-    'success': 'ok',
-  });
+  // res.json({
+  //   'success': 'ok',
+  // });
 })
 
 app.get('/tech-tips', (req, res) => {
   const payload = req.body;
   io.emit('tech-tips', { payload });
-  res.json({
-    'success': 'ok'
-  });
+  // res.json({
+  //   'success': 'ok'
+  // });
 })
 
 
